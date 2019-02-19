644903587228ada7e59af4e2032a16de68ecd609
katherinemariexp <kmsgonzales@gmail.com>
2019-02-19 01:48:41 +0000

    Updated variables/ final submission

--- Car.h
+++ Car.h
@@ -1,6 +1,5 @@
 #ifndef CAR_H
 #define CAR_H
-#include <iostream>
 #include <string>
 
 using namespace std;
--- testCar.cpp
+++ testCar.cpp
@@ -7,19 +7,18 @@ using namespace std;
 int main()
 {
     Car car(2018, "Honda");
-    int speed, count, numOfCalls = 5;
+    int speed;
     
     cout << "Current speed: " << car.getSpeed() << endl;
  
-    
-    for (count = 0; count < numOfCalls; count ++)
+    for (int i = 0; i < 5; i++)
     {
         car.accelerate(speed);
         cout << "Accelerating..." << endl;
         cout << "Current speed: " << car.getSpeed() << endl;
     }
 
-    for (count = 0; count < numOfCalls; count++)
+    for (int i = 0; i < 5; i++)
     {
         car.brake(speed);
         cout << "Braking..." << endl;
